57c91eabe0ca2c5ba4d98fd243f1a228d749018b
Jihong Zhang <jihong-zhang@uiowa.edu>
2019-06-26 17:29:36 -0500

    adjust NAMESPACE

--- R/StanDCM.fit.R
+++ R/StanDCM.fit.R
@@ -11,28 +11,6 @@
 #' @author {Zhehan Jiang, University of Alabama, \email{zjiang17@@ua.edu}}
 #'
 #' @export
-#loading needed packages
-
-# load("data.RData")
-# Qmatrix<-cbind(Qmatrix,rep(1,9)); Qmatrix[1,1]<-0
-# # dim(respMatrix)
-# misspecifiedQmatrix <- Qmatrix
-# misspecifiedQmatrix[1:6,] <- 1-Qmatrix[1:6,]
-#
-# # misspecifiedQmatrix[1,3] = 0
-# #
-
-# estimatedMod_misspecified<-StanLCDM.run(misspecifiedQmatrix,respMatrix, iter=100,init.list='cdm',chains = 3)
-#
-# loo1 <- loo(estimatedMod, pars = "log_lik", save_psis = TRUE)
-# loo2 <- loo(estimatedMod_misspecified, pars = "log_lik", save_psis = TRUE)
-# print(loo1)
-# print(loo2)
-#
-# save(Qmatrix, misspecifiedQmatrix, respMatrix, file = "R/data.RData")
-# save(estimatedMod, estimatedMod_misspecified, file = "R/modelfit.RData")
-#
-# loo::compare(loo1, loo2)
 
 StanDCM.fit <- function(x, pars ="log_lik", cores = 2, save_psis =TRUE) {
   loo1 <- loo(x = x, pars = pars, save_psis = save_psis, cores = cores)
